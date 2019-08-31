c0b5978ca201624ea3e5c423eb595f2c333dc317
Takayuki Sei <takayuki.sei@gree.net>
2019-08-31 13:46:34 +0900

    initial commit

---
+++ DemoStereoMicInput/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoStereoMicInput
+//
+//  Created by Takayuki Sei on 2019/08/31.
+//  Copyright © 2019 net.tionlow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
